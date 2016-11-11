eabcb916e65a3956e354458401ac43b4badccfcb
OlgaTyurina <brkte@bk.ru>
2016-11-11 13:55:08 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
   res.json({
-    hello: 'JS World',
+    hello: 'Jscript World',
   });
 });
+
+app.get('/task2A', (req, res) => {
+  const sum = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(sum.toString());
+});
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
